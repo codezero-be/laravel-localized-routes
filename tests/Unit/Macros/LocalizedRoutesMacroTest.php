55995d4f61e65308239c29e8d7b0c72d8e86d06e
Ivan Vermeyen <ivan@vermeyen.org>
2018-04-03 18:56:28 +0200

    Test localized root route registration

--- tests/Unit/Macros/LocalizedRoutesMacroTest.php
+++ tests/Unit/Macros/LocalizedRoutesMacroTest.php
@@ -5,6 +5,7 @@ namespace CodeZero\LocalizedRoutes\Tests\Unit\Macros;
 use App;
 use CodeZero\LocalizedRoutes\Tests\TestCase;
 use Config;
+use Illuminate\Support\Collection;
 use Route;
 
 class LocalizedRoutesMacroTest extends TestCase
@@ -12,6 +13,14 @@ class LocalizedRoutesMacroTest extends TestCase
     protected function setAvailableLocales($locales)
     {
         Config::set('localized-routes.supported-locales', $locales);
+    }
+
+    protected function getRoutes()
+    {
+        // Route::has() doesn't seem to be working
+        // when you create routes on the fly.
+        // So this is a bit of a workaround...
+        return new Collection(Route::getRoutes());
     }
 
     /** @test */
@@ -24,10 +33,7 @@ class LocalizedRoutesMacroTest extends TestCase
                 ->name('route.name');
         });
 
-        // Route::has() doesn't seem to be working
-        // when you create routes on the fly.
-        // So this is a bit of a workaround...
-        $routes = collect(Route::getRoutes());
+        $routes = $this->getRoutes();
         $names = $routes->pluck('action.as');
         $uris = $routes->pluck('uri');
 
@@ -38,6 +44,29 @@ class LocalizedRoutesMacroTest extends TestCase
         $this->assertNotContains('route', $uris);
         $this->assertContains('en/route', $uris);
         $this->assertContains('nl/route', $uris);
+    }
+
+    /** @test */
+    public function it_registers_a_root_route_for_each_locale()
+    {
+        $this->setAvailableLocales(['en', 'nl']);
+
+        Route::localized(function () {
+            Route::get('/', function () {})
+                ->name('home');
+        });
+
+        $routes = $this->getRoutes();
+        $names = $routes->pluck('action.as');
+        $uris = $routes->pluck('uri');
+
+        $this->assertNotContains('home', $names);
+        $this->assertContains('en.home', $names);
+        $this->assertContains('nl.home', $names);
+
+        $this->assertNotContains('/', $uris);
+        $this->assertContains('en', $uris);
+        $this->assertContains('nl', $uris);
     }
 
     /** @test */
