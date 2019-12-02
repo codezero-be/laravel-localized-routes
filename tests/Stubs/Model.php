5074e4971b2579f8a415aed2bc3d850f08d182f4
Ivan Vermeyen <ivan@vermeyen.org>
2019-12-02 19:21:12 +0100

    Refactor test Model

--- tests/Stubs/Model.php
+++ tests/Stubs/Model.php
@@ -3,16 +3,28 @@
 namespace CodeZero\LocalizedRoutes\Tests\Stubs;
 
 use Illuminate\Database\Eloquent\Model as BaseModel;
+use Illuminate\Support\Facades\App;
 
 class Model extends BaseModel
 {
+    /**
+     * Fake localized slugs.
+     *
+     * @var array
+     */
+    protected $slugs = [
+        'en' => 'en-slug',
+        'nl' => 'nl-slug',
+    ];
+
+    /**
+     * Fake localized route key.
+     *
+     * @return mixed
+     */
     public function getRouteKey()
     {
-        $slugs = [
-            'en' => 'en-slug',
-            'nl' => 'nl-slug',
-        ];
+        return $this->slugs[App::getLocale()];
 
-        return $slugs[app()->getLocale()];
     }
 }
