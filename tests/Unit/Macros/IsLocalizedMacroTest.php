f880f25a79b8e18be50e931a50ea9d335a0c6209
Ivan Vermeyen <ivan@vermeyen.org>
2021-08-16 16:11:36 +0200

    Return string from test requests

--- tests/Unit/Macros/IsLocalizedMacroTest.php
+++ tests/Unit/Macros/IsLocalizedMacroTest.php
@@ -15,25 +15,25 @@ class IsLocalizedMacroTest extends TestCase
 
         Route::localized(function () {
             Route::get('localized-route', function () {
-                return Route::isLocalized();
+                return Route::isLocalized() ? 'true' : 'false';
             })->middleware(['web']);
         });
 
         Route::get('non-localized-route', function () {
-            return Route::isLocalized();
+            return Route::isLocalized() ? 'true' : 'false';
         })->middleware(['web']);
 
         $response = $this->call('GET', '/en/localized-route');
         $response->assertOk();
-        $this->assertEquals(true, $response->original);
+        $this->assertEquals('true', $response->original);
 
         $response = $this->call('GET', '/nl/localized-route');
         $response->assertOk();
-        $this->assertEquals(true, $response->original);
+        $this->assertEquals('true', $response->original);
 
         $response = $this->call('GET', '/non-localized-route');
         $response->assertOk();
-        $this->assertEquals(false, $response->original);
+        $this->assertEquals('false', $response->original);
     }
 
     /** @test */
@@ -44,29 +44,29 @@ class IsLocalizedMacroTest extends TestCase
 
         Route::localized(function () {
             Route::get('route-one', function () {
-                return Route::isLocalized('route-one');
+                return Route::isLocalized('route-one') ? 'true' : 'false';
             })->name('route-one')->middleware(['web']);
 
             Route::get('route-two', function () {
-                return Route::isLocalized('route-one');
+                return Route::isLocalized('route-one') ? 'true' : 'false';
             })->name('route-two')->middleware(['web']);
         });
 
         $response = $this->call('GET', '/en/route-one');
         $response->assertOk();
-        $this->assertEquals(true, $response->original);
+        $this->assertEquals('true', $response->original);
 
         $response = $this->call('GET', '/nl/route-one');
         $response->assertOk();
-        $this->assertEquals(true, $response->original);
+        $this->assertEquals('true', $response->original);
 
         $response = $this->call('GET', '/en/route-two');
         $response->assertOk();
-        $this->assertEquals(false, $response->original);
+        $this->assertEquals('false', $response->original);
 
         $response = $this->call('GET', '/nl/route-two');
         $response->assertOk();
-        $this->assertEquals(false, $response->original);
+        $this->assertEquals('false', $response->original);
     }
 
     /** @test */
@@ -77,17 +77,17 @@ class IsLocalizedMacroTest extends TestCase
 
         Route::localized(function () {
             Route::get('route-one', function () {
-                return Route::isLocalized('route-one', 'en');
+                return Route::isLocalized('route-one', 'en') ? 'true' : 'false';
             })->name('route-one')->middleware(['web']);
         });
 
         $response = $this->call('GET', '/en/route-one');
         $response->assertOk();
-        $this->assertEquals(true, $response->original);
+        $this->assertEquals('true', $response->original);
 
         $response = $this->call('GET', '/nl/route-one');
         $response->assertOk();
-        $this->assertEquals(false, $response->original);
+        $this->assertEquals('false', $response->original);
     }
 
     /** @test */
@@ -98,41 +98,41 @@ class IsLocalizedMacroTest extends TestCase
 
         Route::localized(function () {
             Route::get('route-one', function () {
-                return Route::isLocalized(['route-one', 'route-two']);
+                return Route::isLocalized(['route-one', 'route-two']) ? 'true' : 'false';
             })->name('route-one')->middleware(['web']);
 
             Route::get('route-two', function () {
-                return Route::isLocalized(['route-one', 'route-two']);
+                return Route::isLocalized(['route-one', 'route-two']) ? 'true' : 'false';
             })->name('route-two')->middleware(['web']);
 
             Route::get('route-three', function () {
-                return Route::isLocalized(['route-one', 'route-two']);
+                return Route::isLocalized(['route-one', 'route-two']) ? 'true' : 'false';
             })->name('route-three')->middleware(['web']);
         });
 
         $response = $this->call('GET', '/en/route-one');
         $response->assertOk();
-        $this->assertEquals(true, $response->original);
+        $this->assertEquals('true', $response->original);
 
         $response = $this->call('GET', '/nl/route-one');
         $response->assertOk();
-        $this->assertEquals(true, $response->original);
+        $this->assertEquals('true', $response->original);
 
         $response = $this->call('GET', '/en/route-two');
         $response->assertOk();
-        $this->assertEquals(true, $response->original);
+        $this->assertEquals('true', $response->original);
 
         $response = $this->call('GET', '/nl/route-two');
         $response->assertOk();
-        $this->assertEquals(true, $response->original);
+        $this->assertEquals('true', $response->original);
 
         $response = $this->call('GET', '/en/route-three');
         $response->assertOk();
-        $this->assertEquals(false, $response->original);
+        $this->assertEquals('false', $response->original);
 
         $response = $this->call('GET', '/nl/route-three');
         $response->assertOk();
-        $this->assertEquals(false, $response->original);
+        $this->assertEquals('false', $response->original);
     }
 
     /** @test */
@@ -143,41 +143,41 @@ class IsLocalizedMacroTest extends TestCase
 
         Route::localized(function () {
             Route::get('route-one', function () {
-                return Route::isLocalized(['route-one', 'route-two'], 'en');
+                return Route::isLocalized(['route-one', 'route-two'], 'en') ? 'true' : 'false';
             })->name('route-one')->middleware(['web']);
 
             Route::get('route-two', function () {
-                return Route::isLocalized(['route-one', 'route-two'], 'en');
+                return Route::isLocalized(['route-one', 'route-two'], 'en') ? 'true' : 'false';
             })->name('route-two')->middleware(['web']);
 
             Route::get('route-three', function () {
-                return Route::isLocalized(['route-one', 'route-two'], 'en');
+                return Route::isLocalized(['route-one', 'route-two'], 'en') ? 'true' : 'false';
             })->name('route-three')->middleware(['web']);
         });
 
         $response = $this->call('GET', '/en/route-one');
         $response->assertOk();
-        $this->assertEquals(true, $response->original);
+        $this->assertEquals('true', $response->original);
 
         $response = $this->call('GET', '/nl/route-one');
         $response->assertOk();
-        $this->assertEquals(false, $response->original);
+        $this->assertEquals('false', $response->original);
 
         $response = $this->call('GET', '/en/route-two');
         $response->assertOk();
-        $this->assertEquals(true, $response->original);
+        $this->assertEquals('true', $response->original);
 
         $response = $this->call('GET', '/nl/route-two');
         $response->assertOk();
-        $this->assertEquals(false, $response->original);
+        $this->assertEquals('false', $response->original);
 
         $response = $this->call('GET', '/en/route-three');
         $response->assertOk();
-        $this->assertEquals(false, $response->original);
+        $this->assertEquals('false', $response->original);
 
         $response = $this->call('GET', '/nl/route-three');
         $response->assertOk();
-        $this->assertEquals(false, $response->original);
+        $this->assertEquals('false', $response->original);
     }
 
     /** @test */
@@ -188,36 +188,36 @@ class IsLocalizedMacroTest extends TestCase
 
         Route::localized(function () {
             Route::get('route-one', function () {
-                return Route::isLocalized('route-one', ['en', 'nl']);
+                return Route::isLocalized('route-one', ['en', 'nl']) ? 'true' : 'false';
             })->name('route-one')->middleware(['web']);
 
             Route::get('route-two', function () {
-                return Route::isLocalized('route-one', ['en', 'nl']);
+                return Route::isLocalized('route-one', ['en', 'nl']) ? 'true' : 'false';
             })->name('route-two')->middleware(['web']);
         });
 
         $response = $this->call('GET', '/en/route-one');
         $response->assertOk();
-        $this->assertEquals(true, $response->original);
+        $this->assertEquals('true', $response->original);
 
         $response = $this->call('GET', '/nl/route-one');
         $response->assertOk();
-        $this->assertEquals(true, $response->original);
+        $this->assertEquals('true', $response->original);
 
         $response = $this->call('GET', '/fr/route-one');
         $response->assertOk();
-        $this->assertEquals(false, $response->original);
+        $this->assertEquals('false', $response->original);
 
         $response = $this->call('GET', '/en/route-two');
         $response->assertOk();
-        $this->assertEquals(false, $response->original);
+        $this->assertEquals('false', $response->original);
 
         $response = $this->call('GET', '/nl/route-two');
         $response->assertOk();
-        $this->assertEquals(false, $response->original);
+        $this->assertEquals('false', $response->original);
 
         $response = $this->call('GET', '/fr/route-two');
         $response->assertOk();
-        $this->assertEquals(false, $response->original);
+        $this->assertEquals('false', $response->original);
     }
 }
