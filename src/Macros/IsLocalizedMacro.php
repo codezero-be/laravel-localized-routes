85ab5ba65361ec67e648d351326cdeecfb62ef67
Ivan Vermeyen <ivan@vermeyen.org>
2021-08-16 15:54:13 +0200

    Implement expected Route::is() behavior with Route::isLocalized()

--- src/Macros/IsLocalizedMacro.php
+++ src/Macros/IsLocalizedMacro.php
@@ -3,6 +3,7 @@
 namespace CodeZero\LocalizedRoutes\Macros;
 
 use CodeZero\LocalizedRoutes\LocalizedUrlGenerator;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 
@@ -15,8 +16,21 @@ class IsLocalizedMacro
      */
     public static function register()
     {
-        Route::macro('isLocalized', function () {
-            return App::make(LocalizedUrlGenerator::class)->isLocalized();
+        Route::macro('isLocalized', function ($patterns = null, $locales = '*') {
+            if ($patterns === null) {
+                return App::make(LocalizedUrlGenerator::class)->isLocalized();
+            }
+
+            $locales = Collection::make($locales);
+            $names = Collection::make();
+
+            Collection::make($patterns)->each(function ($name) use ($locales, $names) {
+                Collection::make($locales)->each(function ($locale) use ($name, $names) {
+                    $names->push($locale . '.' . $name);
+                });
+            });
+
+            return Route::is($names->all());
         });
     }
 }
---
+++ tests/Unit/Macros/IsLocalizedMacroTest.php
@@ -0,0 +1,223 @@
+<?php
+
+namespace CodeZero\LocalizedRoutes\Tests\Unit\Macros;
+
+use CodeZero\LocalizedRoutes\Tests\TestCase;
+use Illuminate\Support\Facades\Route;
+
+class IsLocalizedMacroTest extends TestCase
+{
+    /** @test */
+    public function it_checks_if_the_current_route_is_localized()
+    {
+        $this->withoutExceptionHandling();
+        $this->setSupportedLocales(['en', 'nl']);
+
+        Route::localized(function () {
+            Route::get('localized-route', function () {
+                return Route::isLocalized();
+            })->middleware(['web']);
+        });
+
+        Route::get('non-localized-route', function () {
+            return Route::isLocalized();
+        })->middleware(['web']);
+
+        $response = $this->call('GET', '/en/localized-route');
+        $response->assertOk();
+        $this->assertEquals(true, $response->original);
+
+        $response = $this->call('GET', '/nl/localized-route');
+        $response->assertOk();
+        $this->assertEquals(true, $response->original);
+
+        $response = $this->call('GET', '/non-localized-route');
+        $response->assertOk();
+        $this->assertEquals(false, $response->original);
+    }
+
+    /** @test */
+    public function it_checks_if_the_current_route_has_a_name_with_any_locale_prefix()
+    {
+        $this->withoutExceptionHandling();
+        $this->setSupportedLocales(['en', 'nl']);
+
+        Route::localized(function () {
+            Route::get('route-one', function () {
+                return Route::isLocalized('route-one');
+            })->name('route-one')->middleware(['web']);
+
+            Route::get('route-two', function () {
+                return Route::isLocalized('route-one');
+            })->name('route-two')->middleware(['web']);
+        });
+
+        $response = $this->call('GET', '/en/route-one');
+        $response->assertOk();
+        $this->assertEquals(true, $response->original);
+
+        $response = $this->call('GET', '/nl/route-one');
+        $response->assertOk();
+        $this->assertEquals(true, $response->original);
+
+        $response = $this->call('GET', '/en/route-two');
+        $response->assertOk();
+        $this->assertEquals(false, $response->original);
+
+        $response = $this->call('GET', '/nl/route-two');
+        $response->assertOk();
+        $this->assertEquals(false, $response->original);
+    }
+
+    /** @test */
+    public function it_checks_if_the_current_route_has_a_name_with_a_specific_locale_prefix()
+    {
+        $this->withoutExceptionHandling();
+        $this->setSupportedLocales(['en', 'nl']);
+
+        Route::localized(function () {
+            Route::get('route-one', function () {
+                return Route::isLocalized('route-one', 'en');
+            })->name('route-one')->middleware(['web']);
+        });
+
+        $response = $this->call('GET', '/en/route-one');
+        $response->assertOk();
+        $this->assertEquals(true, $response->original);
+
+        $response = $this->call('GET', '/nl/route-one');
+        $response->assertOk();
+        $this->assertEquals(false, $response->original);
+    }
+
+    /** @test */
+    public function it_checks_if_the_current_route_has_a_name_that_is_in_an_array_of_names_with_any_locale_prefix()
+    {
+        $this->withoutExceptionHandling();
+        $this->setSupportedLocales(['en', 'nl']);
+
+        Route::localized(function () {
+            Route::get('route-one', function () {
+                return Route::isLocalized(['route-one', 'route-two']);
+            })->name('route-one')->middleware(['web']);
+
+            Route::get('route-two', function () {
+                return Route::isLocalized(['route-one', 'route-two']);
+            })->name('route-two')->middleware(['web']);
+
+            Route::get('route-three', function () {
+                return Route::isLocalized(['route-one', 'route-two']);
+            })->name('route-three')->middleware(['web']);
+        });
+
+        $response = $this->call('GET', '/en/route-one');
+        $response->assertOk();
+        $this->assertEquals(true, $response->original);
+
+        $response = $this->call('GET', '/nl/route-one');
+        $response->assertOk();
+        $this->assertEquals(true, $response->original);
+
+        $response = $this->call('GET', '/en/route-two');
+        $response->assertOk();
+        $this->assertEquals(true, $response->original);
+
+        $response = $this->call('GET', '/nl/route-two');
+        $response->assertOk();
+        $this->assertEquals(true, $response->original);
+
+        $response = $this->call('GET', '/en/route-three');
+        $response->assertOk();
+        $this->assertEquals(false, $response->original);
+
+        $response = $this->call('GET', '/nl/route-three');
+        $response->assertOk();
+        $this->assertEquals(false, $response->original);
+    }
+
+    /** @test */
+    public function it_checks_if_the_current_route_has_a_name_that_is_in_an_array_of_names_with_a_specific_locale_prefix()
+    {
+        $this->withoutExceptionHandling();
+        $this->setSupportedLocales(['en', 'nl']);
+
+        Route::localized(function () {
+            Route::get('route-one', function () {
+                return Route::isLocalized(['route-one', 'route-two'], 'en');
+            })->name('route-one')->middleware(['web']);
+
+            Route::get('route-two', function () {
+                return Route::isLocalized(['route-one', 'route-two'], 'en');
+            })->name('route-two')->middleware(['web']);
+
+            Route::get('route-three', function () {
+                return Route::isLocalized(['route-one', 'route-two'], 'en');
+            })->name('route-three')->middleware(['web']);
+        });
+
+        $response = $this->call('GET', '/en/route-one');
+        $response->assertOk();
+        $this->assertEquals(true, $response->original);
+
+        $response = $this->call('GET', '/nl/route-one');
+        $response->assertOk();
+        $this->assertEquals(false, $response->original);
+
+        $response = $this->call('GET', '/en/route-two');
+        $response->assertOk();
+        $this->assertEquals(true, $response->original);
+
+        $response = $this->call('GET', '/nl/route-two');
+        $response->assertOk();
+        $this->assertEquals(false, $response->original);
+
+        $response = $this->call('GET', '/en/route-three');
+        $response->assertOk();
+        $this->assertEquals(false, $response->original);
+
+        $response = $this->call('GET', '/nl/route-three');
+        $response->assertOk();
+        $this->assertEquals(false, $response->original);
+    }
+
+    /** @test */
+    public function it_checks_if_the_current_route_has_a_name_with_a_locale_prefix_in_an_array()
+    {
+        $this->withoutExceptionHandling();
+        $this->setSupportedLocales(['en', 'nl', 'fr']);
+
+        Route::localized(function () {
+            Route::get('route-one', function () {
+                return Route::isLocalized('route-one', ['en', 'nl']);
+            })->name('route-one')->middleware(['web']);
+
+            Route::get('route-two', function () {
+                return Route::isLocalized('route-one', ['en', 'nl']);
+            })->name('route-two')->middleware(['web']);
+        });
+
+        $response = $this->call('GET', '/en/route-one');
+        $response->assertOk();
+        $this->assertEquals(true, $response->original);
+
+        $response = $this->call('GET', '/nl/route-one');
+        $response->assertOk();
+        $this->assertEquals(true, $response->original);
+
+        $response = $this->call('GET', '/fr/route-one');
+        $response->assertOk();
+        $this->assertEquals(false, $response->original);
+
+        $response = $this->call('GET', '/en/route-two');
+        $response->assertOk();
+        $this->assertEquals(false, $response->original);
+
+        $response = $this->call('GET', '/nl/route-two');
+        $response->assertOk();
+        $this->assertEquals(false, $response->original);
+
+        $response = $this->call('GET', '/fr/route-two');
+        $response->assertOk();
+        $this->assertEquals(false, $response->original);
+    }
+}
