8c49b230f30e1edb2fdebb09981257cca11c8c4e
Ivan Vermeyen <ivan@vermeyen.org>
2023-03-16 22:44:14 +0100

    Centralize configuration queries to a dedicated class

--- src/LocalizedRoutesRegistrar.php
+++ src/LocalizedRoutesRegistrar.php
@@ -2,10 +2,9 @@
 
 namespace CodeZero\LocalizedRoutes;
 
+use CodeZero\LocalizedRoutes\Facades\LocaleConfig;
 use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Str;
 
 class LocalizedRoutesRegistrar
 {
@@ -19,21 +18,23 @@ class LocalizedRoutesRegistrar
      */
     public function register($closure, $options = [])
     {
-        $locales = $options['supported_locales'] ?? Config::get('localized-routes.supported_locales', []);
-        $omitPrefix = $options['omitted_locale'] ?? Config::get('localized-routes.omitted_locale');
-        $localeRouteAction = Config::get('localized-routes.route_action');
+        $locales = $options['supported_locales'] ?? LocaleConfig::getSupportedLocales();
+        $omittedLocale = $options['omitted_locale'] ?? LocaleConfig::getOmittedLocale();
 
         if (count($locales) === 0) {
             return;
         }
 
-        $firstValue = array_values($locales)[0];
-        $usingDomains = Str::contains($firstValue, '.');
-        $usingCustomSlugs = ! $usingDomains && ! is_numeric(key($locales));
+        LocaleConfig::setSupportedLocales($locales);
+        LocaleConfig::setOmittedLocale($omittedLocale);
+
+        $localeRouteAction = LocaleConfig::getRouteAction();
+        $usingDomains = LocaleConfig::hasCustomDomains();
+        $usingCustomSlugs = LocaleConfig::hasCustomSlugs();
 
         // Move the omitted locale to the end of the array to avoid
         // root parameter placeholders catching hard-coded slugs.
-        $locales = $this->moveOmittedLocaleToEnd($locales, $omitPrefix, $usingDomains, $usingCustomSlugs);
+        $locales = $this->moveOmittedLocaleToEnd($locales, $omittedLocale, $usingDomains, $usingCustomSlugs);
 
         // Remember the current locale, so we can
         // change it during route registration.
@@ -62,7 +63,7 @@ class LocalizedRoutesRegistrar
 
             // Add a URL prefix to the route group, unless
             // the locale is configured to be omitted.
-            if ( ! $usingDomains && $locale !== $omitPrefix) {
+            if ( ! $usingDomains && $locale !== $omittedLocale) {
                 $attributes['prefix'] = $domainOrSlug;
             }
 
@@ -83,35 +84,36 @@ class LocalizedRoutesRegistrar
      * Move the omitted locale to the end of the locales array.
      *
      * @param array $locales
-     * @param string|null $omitPrefix
+     * @param string|null $omittedLocale
      * @param bool $usingDomains
      * @param bool $usingCustomSlugs
      *
      * @return array
      */
-    protected function moveOmittedLocaleToEnd($locales, $omitPrefix, $usingDomains, $usingCustomSlugs)
+    protected function moveOmittedLocaleToEnd($locales, $omittedLocale, $usingDomains, $usingCustomSlugs)
     {
-        if ( ! $omitPrefix || $usingDomains) {
+        if ( ! $omittedLocale || $usingDomains) {
             return $locales;
         }
 
         // When using custom slugs, the locales are the array keys.
-        // Remove it from the array and add it back on to the end.
+        // Remove the omitted locale from the array
+        // and add it back on to the end.
         if ($usingCustomSlugs) {
-            $omitSlug = $locales[$omitPrefix];
-            unset($locales[$omitPrefix]);
-            $locales[$omitPrefix] = $omitSlug;
+            $omitSlug = $locales[$omittedLocale];
+            unset($locales[$omittedLocale]);
+            $locales[$omittedLocale] = $omitSlug;
 
             return $locales;
         }
 
-        // When not using custom slugs, the array keys are numeric.
+        // When not using custom slugs or domains, the array keys are numeric.
         // Filter out the omitted locale and then add it back to the end.
-        $locales = array_filter($locales, function ($locale) use ($omitPrefix) {
-            return $locale !== $omitPrefix;
+        $locales = array_filter($locales, function ($locale) use ($omittedLocale) {
+            return $locale !== $omittedLocale;
         });
 
-        $locales[] = $omitPrefix;
+        $locales[] = $omittedLocale;
 
         return $locales;
     }
---
+++ tests/Unit/LocaleConfigTest.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace CodeZero\LocalizedRoutes\Tests\Unit;
+
+use CodeZero\LocalizedRoutes\LocaleConfig;
+use CodeZero\LocalizedRoutes\Tests\TestCase;
+
+class LocaleConfigTest extends TestCase
+{
+    /** @test */
+    public function it_gets_the_supported_locales()
+    {
+        $config = new LocaleConfig(['supported_locales' => null]);
+        $this->assertEquals([], $config->getSupportedLocales());
+
+        $config = new LocaleConfig(['supported_locales' => ['en', 'nl']]);
+        $this->assertEquals(['en', 'nl'], $config->getSupportedLocales());
+
+        $config = new LocaleConfig(['supported_locales' => ['en' => 'english', 'nl' => 'dutch']]);
+        $this->assertEquals(['en' => 'english', 'nl' => 'dutch'], $config->getSupportedLocales());
+
+        $config = new LocaleConfig(['supported_locales' => ['en' => 'english.test', 'nl' => 'dutch.test']]);
+        $this->assertEquals(['en' => 'english.test', 'nl' => 'dutch.test'], $config->getSupportedLocales());
+    }
+
+    /** @test */
+    public function it_sets_the_supported_locales()
+    {
+        $config = new LocaleConfig(['supported_locales' => null]);
+        $config->setSupportedLocales(['en' => 'english', 'nl' => 'dutch']);
+        $this->assertEquals(['en' => 'english', 'nl' => 'dutch'], $config->getSupportedLocales());
+    }
+
+    /** @test */
+    public function it_gets_the_omitted_locale()
+    {
+        $config = new LocaleConfig(['omitted_locale' => null]);
+        $this->assertEquals(null, $config->getOmittedLocale());
+
+        $config = new LocaleConfig(['omitted_locale' => 'en']);
+        $this->assertEquals('en', $config->getOmittedLocale());
+    }
+
+    /** @test */
+    public function it_sets_the_omitted_locale()
+    {
+        $config = new LocaleConfig(['omitted_locale' => null]);
+        $config->setOmittedLocale('en');
+        $this->assertEquals('en', $config->getOmittedLocale());
+    }
+
+    /** @test */
+    public function it_gets_the_fallback_locale()
+    {
+        $config = new LocaleConfig(['fallback_locale' => null]);
+        $this->assertEquals(null, $config->getFallbackLocale());
+
+        $config = new LocaleConfig(['fallback_locale' => 'en']);
+        $this->assertEquals('en', $config->getFallbackLocale());
+    }
+
+    /** @test */
+    public function it_sets_the_fallback_locale()
+    {
+        $config = new LocaleConfig(['fallback_locale' => null]);
+        $config->setFallbackLocale('en');
+        $this->assertEquals('en', $config->getFallbackLocale());
+    }
+
+    /** @test */
+    public function it_gets_the_route_action()
+    {
+        $config = new LocaleConfig(['route_action' => null]);
+        $this->assertEquals(null, $config->getRouteAction());
+
+        $config = new LocaleConfig(['route_action' => 'locale']);
+        $this->assertEquals('locale', $config->getRouteAction());
+    }
+
+    /** @test */
+    public function it_sets_the_route_action()
+    {
+        $config = new LocaleConfig(['route_action' => null]);
+        $config->setRouteAction('locale');
+        $this->assertEquals('locale', $config->getRouteAction());
+    }
+
+    /** @test */
+    public function it_gets_the_locales()
+    {
+        $config = new LocaleConfig(['supported_locales' => null]);
+        $this->assertEquals([], $config->getLocales());
+
+        $config = new LocaleConfig(['supported_locales' => ['en', 'nl']]);
+        $this->assertEquals(['en', 'nl'], $config->getLocales());
+
+        $config = new LocaleConfig(['supported_locales' => ['en' => 'english', 'nl' => 'dutch']]);
+        $this->assertEquals(['en', 'nl'], $config->getLocales());
+
+        $config = new LocaleConfig(['supported_locales' => ['en' => 'english.test', 'nl' => 'dutch.test']]);
+        $this->assertEquals(['en', 'nl'], $config->getLocales());
+    }
+
+    /** @test */
+    public function it_checks_if_there_are_any_locales_configured()
+    {
+        $config = new LocaleConfig(['supported_locales' => null]);
+        $this->assertFalse($config->hasLocales());
+
+        $config = new LocaleConfig(['supported_locales' => ['en', 'nl']]);
+        $this->assertTrue($config->hasLocales());
+
+        $config = new LocaleConfig(['supported_locales' => ['en' => 'english', 'nl' => 'dutch']]);
+        $this->assertTrue($config->hasLocales());
+
+        $config = new LocaleConfig(['supported_locales' => ['en' => 'english.test', 'nl' => 'dutch.test']]);
+        $this->assertTrue($config->hasLocales());
+    }
+
+    /** @test */
+    public function it_checks_if_custom_slugs_are_configured()
+    {
+        $config = new LocaleConfig(['supported_locales' => null]);
+        $this->assertFalse($config->hasCustomSlugs());
+
+        $config = new LocaleConfig(['supported_locales' => ['en', 'nl']]);
+        $this->assertFalse($config->hasCustomSlugs());
+
+        $config = new LocaleConfig(['supported_locales' => ['en' => 'english', 'nl' => 'dutch']]);
+        $this->assertTrue($config->hasCustomSlugs());
+
+        $config = new LocaleConfig(['supported_locales' => ['en' => 'english.test', 'nl' => 'dutch.test']]);
+        $this->assertFalse($config->hasCustomSlugs());
+    }
+
+    /** @test */
+    public function it_checks_if_custom_domains_are_configured()
+    {
+        $config = new LocaleConfig(['supported_locales' => null]);
+        $this->assertFalse($config->hasCustomDomains());
+
+        $config = new LocaleConfig(['supported_locales' => ['en', 'nl']]);
+        $this->assertFalse($config->hasCustomDomains());
+
+        $config = new LocaleConfig(['supported_locales' => ['en' => 'english', 'nl' => 'dutch']]);
+        $this->assertFalse($config->hasCustomDomains());
+
+        $config = new LocaleConfig(['supported_locales' => ['en' => 'english.test', 'nl' => 'dutch.test']]);
+        $this->assertTrue($config->hasCustomDomains());
+    }
+
+    /** @test */
+    public function it_checks_if_a_locale_is_supported()
+    {
+        $config = new LocaleConfig(['supported_locales' => null]);
+        $this->assertFalse($config->isSupportedLocale(null));
+        $this->assertFalse($config->isSupportedLocale('en'));
+
+        $config = new LocaleConfig(['supported_locales' => ['en']]);
+        $this->assertFalse($config->isSupportedLocale('nl'));
+        $this->assertTrue($config->isSupportedLocale('en'));
+
+        $config = new LocaleConfig(['supported_locales' => ['en' => 'english']]);
+        $this->assertFalse($config->isSupportedLocale('english'));
+        $this->assertTrue($config->isSupportedLocale('en'));
+
+        $config = new LocaleConfig(['supported_locales' => ['en' => 'english.test']]);
+        $this->assertFalse($config->isSupportedLocale('english.test'));
+        $this->assertTrue($config->isSupportedLocale('en'));
+    }
+}
