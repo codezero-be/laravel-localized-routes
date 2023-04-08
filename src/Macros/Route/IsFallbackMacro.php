d977227ede13f58202a07ff8dec5d0239618e236
Ivan Vermeyen <ivan@vermeyen.org>
2023-04-08 15:43:14 +0200

    Inject Request instead of using the facade

--- src/Macros/Route/IsFallbackMacro.php
+++ src/Macros/Route/IsFallbackMacro.php
@@ -2,6 +2,8 @@
 
 namespace CodeZero\LocalizedRoutes\Macros\Route;
 
+use CodeZero\LocalizedRoutes\RouteHelper;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 
 class IsFallbackMacro
@@ -14,7 +16,7 @@ class IsFallbackMacro
     public static function register()
     {
         Route::macro('isFallback', function () {
-            return Route::current() && Route::current()->isFallback;
+            return App::make(RouteHelper::class)->isFallback();
         });
     }
 }
--- src/Middleware/Detectors/UrlDetector.php
+++ src/Middleware/Detectors/UrlDetector.php
@@ -3,10 +3,27 @@
 namespace CodeZero\LocalizedRoutes\Middleware\Detectors;
 
 use CodeZero\LocalizedRoutes\Facades\LocaleConfig;
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
 
 class UrlDetector implements Detector
 {
+    /**
+     * The current Request.
+     *
+     * @var \Illuminate\Http\Request
+     */
+    protected $request;
+
+    /**
+     * Create a new UrlDetector instance.
+     *
+     * @param \Illuminate\Http\Request $request
+     */
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
     /**
      * Detect the locale.
      *
@@ -14,7 +31,7 @@ class UrlDetector implements Detector
      */
     public function detect()
     {
-        $slug = Request::segment(1);
+        $slug = $this->request->segment(1);
 
         // If supported locales is a simple array like ['en', 'nl']
         // just return the slug and let the calling code check if it is supported.
@@ -25,7 +42,7 @@ class UrlDetector implements Detector
         // Find the locale that belongs to the custom domain or slug.
         // Return the original slug as fallback.
         // The calling code should validate and handle it.
-        $domain = Request::getHttpHost();
+        $domain = $this->request->getHttpHost();
         $locale = LocaleConfig::findLocaleByDomain($domain) ?? LocaleConfig::findLocaleBySlug($slug) ?? $slug;
 
         return $locale;
